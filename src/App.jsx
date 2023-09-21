4ed41f240d42a280409f431c3c809c19ef0f5b99
LongarMD <mark.longar@gmail.com>
2023-09-21 14:17:44 +0200

    tailwind prettier plugin

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,11 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="w-full h-full p-4">
-      <button
-        onClick={() => setCount((count) => count + 1)}
-        className="p-3 border-2 rounded-lg border-red-500"
-      >
-        count is {count}
-      </button>
-    </main>
+    <button
+      onClick={() => setCount((count) => count + 1)}
+      className="rounded-lg border-2 border-red-500 p-4 lg:border-green-500"
+    >
+      count is {count}
+    </button>
   );
 }
